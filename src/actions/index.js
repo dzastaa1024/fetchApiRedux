de7eeff78ee5433eb697c109813d265240dc8723
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-05-13 16:31:40 +0100

    refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return async (dispatch) => {
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get("/post");
-
     dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
